296336b4fd759516dd9547150e87d388c70dc451
lkaymakanov <bat_lubo2001@abv.bg>
2016-10-06 15:10:42 +0300

    add template functionality

---
+++ ltfhelp/JavaResource/resources/templates/js/applinks.js
@@ -0,0 +1,61 @@
+﻿
+function AppLinks(){
+	this.applinks;
+	this.thisref;
+	
+	this.createHref = function(linkInfo){
+		return linkInfo.protocol + '://' + linkInfo.ip + ':' + linkInfo.port + '/' + linkInfo.context;
+	};
+	
+	this.selectLink = function(link){
+		if(selectedLinkId!="-1"){
+			document.getElementById(selectedLinkId).style.backgroundColor  = "";
+		}
+		link.style.backgroundColor  = selectColor;
+		selectedLinkId = link.id;
+		document.getElementById('seldb').innerHTML =  link.innerHTML;
+	};
+	
+	this.showToolTip = function (link){
+		var id= 'tooltip_'+ link.dataset.link;
+		var el = document.getElementById(id);
+		el.style.display='block';
+		if(link.id != selectedLinkId){
+			link.style.backgroundColor = highlightColor;
+		}
+		
+	};
+	
+	this.hideToolTip = function (link){
+		var id= 'tooltip_'+ link.dataset.link;
+		var el = document.getElementById(id).style.display='none';
+		var bc = link.style.backgroundColor;
+		if(bc==highlightColor){
+			link.style.backgroundColor = "";
+		}
+	};
+	
+	this.createLink = function (linkInfo, index){
+		var href = this.createHref(linkInfo);
+		var target = "mateus";
+		if(linkInfo.protocol == 'https') target="_top";
+		var tooltTipCnt = 'IP:' + linkInfo.ip + '</br>Port:' + linkInfo.port;
+		var tooltip = '<div id="tooltip_' + index + '" style="position:absolute; font-weight:bold; z-index:1000; border:solid 1px green; display:none; top:0; background-color:yellow; left:100%; ">'+ tooltTipCnt +'</div>';
+		var tr = '<li style="position:relative; width:100px;"><a id="link_' + index + 
+		'" style="text-decoration:none; color:white;" data-link="' + index +'" href="' +
+		href +'"' + 'onclick="'+this.thisref+'.selectLink(this)"  onmouseover="'+this.thisref+'.showToolTip(this)" onmouseout="'+this.thisref+'.hideToolTip(this)"   target="' + target +'" >'+
+		linkInfo.name + '</a>' + tooltip + '</li>';
+		return tr; 
+	};
+
+};
+
+AppLinks.prototype.init_1 = function(thisref, applinks){
+	this.applinks = applinks;
+	this.thisref = thisref;
+	return this;
+
+};
+
+
+	
