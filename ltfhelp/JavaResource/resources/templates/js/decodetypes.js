2fd66a21bf17e547c6fe040e07542690379d8aba
lkaymakanov <bat_lubo2001@abv.bg>
2016-10-07 15:48:51 +0300

    add template functionality

---
+++ ltfhelp/JavaResource/resources/templates/js/decodetypes.js
@@ -0,0 +1,50 @@
+
+
+function createOptions(opts){
+	var opt='';	
+	for(pp=0; pp < opts.length; pp++){	
+	    opt+='<option>' + opts[pp].label + '</option>';
+	}
+	return '<select>' + opt + '</select>';
+}
+
+
+function createTableRow(dtype, index){
+	var cls = ( index%2 == 0) ? 'even': 'odd';
+	var redirectcontent='-----';
+	var rowCnt;
+	rowCnt = '<tr class="' + cls +'">' +
+	'<td>' + dtype.type + '</td>' +  
+	'<td>' + createOptions(dtype.options) + '</td>' +
+	'<td>' + dtype.refcol + '</td>' +'</tr>';
+	return rowCnt;
+}
+
+
+function filterColumn(dtype){
+	var inputSearch = document.getElementById("searchtext").value;
+	if(inputSearch.length < 1) return true;
+	inputSearch = inputSearch.toLowerCase();
+	return dtype.type.toLowerCase().indexOf(inputSearch) > -1;
+};
+
+function recreateTable(){
+	var d = document.getElementById('tableDiv');
+	var searchTxt = document.getElementById('tableDiv');
+	var tableStr='<table class="register">';
+	var header='<tr class="header">' + 
+	'<td>' + 'DecodeType' +  '</td>' + 
+	'<td>' + 'Options' +  '</td>' + 
+	'<td>' + 'Ref columns' +  '</td>' + 
+	'</tr>';
+	tableStr+=header;
+	var row = '';
+	var filtered = decodetypes.filter(filterColumn);
+	for(i=0; i < filtered.length; i++){
+		row = createTableRow(filtered[i], i);
+		tableStr+=row;
+	}
+	//for(i=0; i < decodetypes.length; i++){tableStr+=(createTableRow(decodetypes[i], i));}
+	tableStr+='</table>';
+	d.innerHTML = tableStr;
+}
